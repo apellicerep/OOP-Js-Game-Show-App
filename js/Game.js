bf85761042f9c026f42edfd3862263a23bab8b37
Arturo <arturomailbcn@gmail.com>
2019-09-24 16:44:55 +0200

    GameOver and checkForWin Working

--- js/Game.js
+++ js/Game.js
@@ -17,10 +17,18 @@ class Game {
     }
 
     startGame() {
+
+        //clean before start
+        this.missed = 0
+        document.querySelectorAll('.key').forEach(key => key.className = "key")
+        document.querySelectorAll('.tries img').forEach(img => img.src = "images/liveHeart.png")
+
+
         document.querySelector('#overlay').style.display = "none"
         this.activePhrase = this.getRandomPhrase() //adding random object Phrase in activePhrase.
         this.activePhrase.addPhraseToDisplay()
         console.log(this.activePhrase)
+
 
 
     }
@@ -46,8 +54,7 @@ class Game {
                 if (key.textContent === letter) {
                     key.classList.add("chosen")
                     this.activePhrase.showMatchedLetter(letter)
-                    //this.removeLife()
-                    //this.checkForWin()
+                    this.checkForWin()
                     //this.gameOver()
                 }
 
@@ -57,30 +64,61 @@ class Game {
 
     removeLife() {
 
-        if (this.missed === 4) this.gameOver();
-
         let srcNodeList = document.querySelectorAll('.tries img')
         let src = Array.from(srcNodeList) //nodelist to array because i want to use includes.
 
-        for (let i = 0; i < src.length; i++) {
+        for (let i = 0; i <= src.length; i++) {
             if (src[i].src.includes('live')) {
                 src[i].src = `images/lostHeart.png`
                 break;
             }
         }
+
+        if (this.missed === 4) this.gameOver();
         this.missed++
+
+
     }
 
-    ceckForWin() {
-
-
+    checkForWin() {
+        let letras = document.querySelectorAll('.letter')
+        for (let letters of letras) {
+            if (letters.classList.contains("hide")) return;
+        }
+        setTimeout(() => {
+            document.querySelector('#overlay').style.display = ""
+            document.querySelector('#game-over-message').textContent = "YOU WIN"
+            document.querySelector('#overlay').classList.add("win")
+        }, 1000)
     }
 
     gameOver() {
-        console.log("game over")
+
+        function callInterval() {
+            let test = document.querySelectorAll('.letter')
+            let i = 0;
+            let interval = setInterval(function () {
+                if (i < test.length) {
+                    test[i].classList.replace("hide", "show")
+                    i++;
+                } else {
+                    console.log("stop")
+                    clearInterval(interval)
+                }
+
+            }, 75)
+
+        }
+
+        callInterval();
+
+
+
+        setTimeout(() => {
+            document.querySelector('#overlay').style.display = ""
+            document.querySelector('#game-over-message').textContent = "GAME OVER"
+            document.querySelector('#overlay').classList.add("lose")
+        }, 2300)
 
     }
-
-
-
 }
